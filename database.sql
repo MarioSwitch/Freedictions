e7ab03b75deecfa30b7ed40a2b750bd382619c20
MarioSwitch <druonalexis@outlook.fr>
2023-05-30 16:40:44 +0200

    Beginning v2

---
+++ database.sql
@@ -0,0 +1,42 @@
+CREATE TABLE `users` (
+    `username` varchar(20) NOT NULL,
+    `password` char(60) NOT NULL,
+    `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `streak` int NOT NULL DEFAULT 0,
+    `points` bigint NOT NULL DEFAULT 100,
+    `mod` tinyint(1) NOT NULL DEFAULT 0,
+    PRIMARY KEY (`username`)
+);
+
+CREATE TABLE `predictions` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `title` varchar(255) NOT NULL,
+    `description` varchar(1000) NOT NULL,
+    `user` varchar(20) NOT NULL,
+    `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `ended` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `answer` int NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `pred_user` FOREIGN KEY (`user`) REFERENCES `users` (`username`)
+);
+
+CREATE TABLE `choices` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `prediction` int NOT NULL,
+    `name` varchar(100) NOT NULL,
+    PRIMARY KEY (`id`),
+    CONSTRAINT `prediction` FOREIGN KEY (`prediction`) REFERENCES `predictions` (`id`)
+);
+
+CREATE TABLE `votes` (
+    `user` varchar(20) NOT NULL,
+    `choice` int NOT NULL,
+    `points` bigint NOT NULL,
+    PRIMARY KEY (`user`, `choice`),
+    CONSTRAINT `vote_user` FOREIGN KEY (`user`) REFERENCES `users` (`username`),
+    CONSTRAINT `choice` FOREIGN KEY (`choice`) REFERENCES `choices` (`id`)
+);
+
+ALTER TABLE `predictions` 
+    ADD CONSTRAINT `answer` FOREIGN KEY (`answer`) REFERENCES `choices` (`id`);
--- libs/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-//À compléter pour connecter la BDD
-$BDD_host="127.0.0.1"; //localhost par défaut, à modifier si nécessaire
-$BDD_user="";
-$BDD_password="";
-$BDD_base="";
-?>
