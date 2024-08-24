41f7de96b20bde2a7b9a1f0060a33b6f12acd03c
MarioSwitch <druonalexis@outlook.fr>
2024-08-24 17:29:17 +0200

    [2.1.alpha8] badges

--- views/badges.php
+++ views/badges.php
@@ -1,112 +1,110 @@
-<h1>Badges</h1>
-<p>Ci-dessous, la liste des badges disponibles. Obtenir un badge affichera une icône à côté de votre pseudo sur tout le site !</p>
-<hr>
-<h2>Badges à obtenir</h2>
-<?php 
+<?php
     include_once "badgesManager.php";
+    echo "<h1>" . getString("badges_title") . "</h1>";
+    echo "<p>" . getString("badges_description") . "</p>";
+    echo "<hr>";
+    echo "<h2>" . getString("badges_available") . "</h2>";
     if(isConnected()){
         //Streak
         $streak = intSQL("SELECT `streak` FROM `users` WHERE `username` = ?;", [$_COOKIE["username"]]);
-        $streakCurrentBadgeLevel = getCurrentBadgeLevel($streak, $streak_badges, "jours");
-        $streakNextBadgeLevel = getNextBadgeLevel($streak, $streak_badges, "jours");
+        $streakCurrentBadgeLevel = getCurrentBadgeLevel($streak, $streak_badges, getString("streak_unit"));
+        $streakNextBadgeLevel = getNextBadgeLevel($streak, $streak_badges, getString("streak_unit"));
         //Points
         $points = intSQL("SELECT `points` FROM `users` WHERE `username` = ?;", [$_COOKIE["username"]]);
-        $pointsCurrentBadgeLevel = getCurrentBadgeLevel($points, $points_badges, "points");
-        $pointsNextBadgeLevel = getNextBadgeLevel($points, $points_badges, "points");
+        $pointsCurrentBadgeLevel = getCurrentBadgeLevel($points, $points_badges, getString("points_unit"));
+        $pointsNextBadgeLevel = getNextBadgeLevel($points, $points_badges, getString("points_unit"));
         //Predictions created
         $predictionsCreated = intSQL("SELECT `predictionsCreated` FROM `users` LEFT JOIN (SELECT `user`, COUNT(*) AS `predictionsCreated` FROM `predictions` GROUP BY `user`) `predictions2` ON `users`.`username` = `predictions2`.`user` WHERE `username` = ?;", [$_COOKIE["username"]]);
-        $predictionsCreatedCurrentBadgeLevel = getCurrentBadgeLevel($predictionsCreated, $predictionsCreated_badges, "prédictions");
-        $predictionsCreatedNextBadgeLevel = getNextBadgeLevel($predictionsCreated, $predictionsCreated_badges, "prédictions");
+        $predictionsCreatedCurrentBadgeLevel = getCurrentBadgeLevel($predictionsCreated, $predictionsCreated_badges, getString("predictions_unit"));
+        $predictionsCreatedNextBadgeLevel = getNextBadgeLevel($predictionsCreated, $predictionsCreated_badges, getString("predictions_unit"));
         //Bets
         $bets = intSQL("SELECT `bets` FROM `users` LEFT JOIN (SELECT `user`, COUNT(*) AS `bets` FROM `votes` GROUP BY `user`) `votes2` ON `users`.`username` = `votes2`.`user` WHERE `username` = ?;", [$_COOKIE["username"]]);
-        $betsCurrentBadgeLevel = getCurrentBadgeLevel($bets, $bets_badges, "mises");
-        $betsNextBadgeLevel = getNextBadgeLevel($bets, $bets_badges, "mises");
+        $betsCurrentBadgeLevel = getCurrentBadgeLevel($bets, $bets_badges, getString("votes_unit"));
+        $betsNextBadgeLevel = getNextBadgeLevel($bets, $bets_badges, getString("votes_unit"));
         //Points spent
         $pointsSpent = intSQL("SELECT `pointsSpent` FROM `users` LEFT JOIN (SELECT `user`, SUM(`points`) AS `pointsSpent` FROM `votes` GROUP BY `user`) `votes2` ON `users`.`username` = `votes2`.`user` WHERE `username` = ?;", [$_COOKIE["username"]]);
-        $pointsSpentCurrentBadgeLevel = getCurrentBadgeLevel($pointsSpent, $pointsSpent_badges, "points");
-        $pointsSpentNextBadgeLevel = getNextBadgeLevel($pointsSpent, $pointsSpent_badges, "points");
+        $pointsSpentCurrentBadgeLevel = getCurrentBadgeLevel($pointsSpent, $pointsSpent_badges, getString("points_unit"));
+        $pointsSpentNextBadgeLevel = getNextBadgeLevel($pointsSpent, $pointsSpent_badges, getString("points_unit"));
     }
-?>
-<table>
+    function displayInviteShowProgress(){
+        echo "<td colspan='2'>" . displayInvite(getString("invite_action_show_progress")) . "</td>";
+    }
+ echo "<table>
     <tr>
-        <th>Badge</th>
-        <th>Bronze</th>
-        <th>Argent</th>
-        <th>Or</th>
-        <th>Diamant</th>
-        <th>Palier actuel</th>
-        <th>Palier suivant</th>
+        <th>" . getString("badges_badge") . "</th>
+        <th>" . getString("badges_bronze") . "</th>
+        <th>" . getString("badges_silver") . "</th>
+        <th>" . getString("badges_gold") . "</th>
+        <th>" . getString("badges_diamond") . "</th>
+        <th>" . getString("badges_level_current") . "</th>
+        <th>" . getString("badges_level_next") . "</th>
     </tr>
     <tr>
-        <td>Série de connexion</td>
-        <?php 
-            generateStaticBadgeRow("calendar", $streak_badges, "jours");
-            if(isConnected()){
-                echo "<td>" . $streakCurrentBadgeLevel . "</td>";
-                echo "<td>" . $streakNextBadgeLevel . "</td>";
-            } else {
-                echo "<td colspan='2'>" . displayInvite("voir votre progression") . "</td>";
-            }
-        ?>
+        <td>" . getString("streak") . "</td>";
+        generateStaticBadgeRow("calendar", $streak_badges, getString("streak_unit"));
+        if(isConnected()){
+            echo "<td>" . $streakCurrentBadgeLevel . "</td>";
+            echo "<td>" . $streakNextBadgeLevel . "</td>";
+        } else {
+            displayInviteShowProgress();
+        }
+echo "
     </tr>
     <tr>
-        <td>Points</td>
-        <?php
-            generateDynamicBadgeRow("points", $points_top, $points_badges, "points");
-            if(isConnected()){
-                echo "<td>" . $pointsCurrentBadgeLevel . "</td>";
-                echo "<td>" . $pointsNextBadgeLevel . "</td>";
-            } else {
-                echo "<td colspan='2'>" . displayInvite("voir votre progression") . "</td>";
-            }
-        ?>
+        <td>" . getString("points") . "</td>";
+        generateDynamicBadgeRow("points", $points_top, $points_badges, getString("points_unit"));
+        if(isConnected()){
+            echo "<td>" . $pointsCurrentBadgeLevel . "</td>";
+            echo "<td>" . $pointsNextBadgeLevel . "</td>";
+        } else {
+            displayInviteShowProgress();
+        }
+echo "
     </tr>
     <tr>
-        <td>Prédictions créées</td>
-        <?php
-            generateDynamicBadgeRow("predictionsCreated", $predictionsCreated_top, $predictionsCreated_badges, "prédictions");
-            if(isConnected()){
-                echo "<td>" . $predictionsCreatedCurrentBadgeLevel . "</td>";
-                echo "<td>" . $predictionsCreatedNextBadgeLevel . "</td>";
-            } else {
-                echo "<td colspan='2'>" . displayInvite("voir votre progression") . "</td>";
-            }
-        ?>
+        <td>" . getString("predictions_created_no_value") . "</td>";
+        generateDynamicBadgeRow("predictionsCreated", $predictionsCreated_top, $predictionsCreated_badges, getString("predictions_unit"));
+        if(isConnected()){
+            echo "<td>" . $predictionsCreatedCurrentBadgeLevel . "</td>";
+            echo "<td>" . $predictionsCreatedNextBadgeLevel . "</td>";
+        } else {
+            displayInviteShowProgress();
+        }
+echo "
     </tr>
     <tr>
-        <td>Mises</td>
-        <?php
-            generateDynamicBadgeRow("bets", $bets_top, $bets_badges, "mises");
-            if(isConnected()){
-                echo "<td>" . $betsCurrentBadgeLevel . "</td>";
-                echo "<td>" . $betsNextBadgeLevel . "</td>";
-            } else {
-                echo "<td colspan='2'>" . displayInvite("voir votre progression") . "</td>";
-            }
-        ?>
+        <td>" . getString("votes") . "</td>";
+        generateDynamicBadgeRow("bets", $bets_top, $bets_badges, getString("votes_unit"));
+        if(isConnected()){
+            echo "<td>" . $betsCurrentBadgeLevel . "</td>";
+            echo "<td>" . $betsNextBadgeLevel . "</td>";
+        } else {
+            displayInviteShowProgress();
+        }
+echo "
     </tr>
     <tr>
-        <td>Points dépensés</td>
-        <?php
-            generateDynamicBadgeRow("pointsSpent", $pointsSpent_top, $pointsSpent_badges, "points");
-            if(isConnected()){
-                echo "<td>" . $pointsSpentCurrentBadgeLevel . "</td>";
-                echo "<td>" . $pointsSpentNextBadgeLevel . "</td>";
-            } else {
-                echo "<td colspan='2'>" . displayInvite("voir votre progression") . "</td>";
-            }
-        ?>
+        <td>" . getString("points_spent") . "</td>";
+        generateDynamicBadgeRow("pointsSpent", $pointsSpent_top, $pointsSpent_badges, getString("points_unit"));
+        if(isConnected()){
+            echo "<td>" . $pointsSpentCurrentBadgeLevel . "</td>";
+            echo "<td>" . $pointsSpentNextBadgeLevel . "</td>";
+        } else {
+            displayInviteShowProgress();
+        }
+echo "
     </tr>
 </table>
 <hr>
-<h2>Autres badges</h2>
+<h2>" . getString("badges_other") . "</h2>
 <table>
     <tr>
-        <th>Badge</th>
-        <th>Rôle</th>
+        <th>" . getString("badges_badge") . "</th>
+        <th>" . getString("badges_role") . "</th>
     </tr>
     <tr>
-        <td><img src="svg/mod.png" alt="Modérateur" title="Modérateur"></td>
-        <td>Modérateur du site</td>
+        <td><img src=\"svg/mod.png\" alt=\"" . getString("mod") . "\" title=\"" . getString("mod") . "\"></td>
+        <td>" . getString("mod") . "</td>
     </tr>
-</table>
+</table>";
+?>
