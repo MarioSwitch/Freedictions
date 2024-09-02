aebcf854f56eeb07c166532b0fdb069af23cd127
MarioSwitch <druonalexis@outlook.fr>
2024-09-02 13:51:41 +0200

    [2.1.beta19] Added 2.1 changelog to home page

--- views/home.php
+++ views/home.php
@@ -13,6 +13,14 @@ if(array_key_exists("error",$_REQUEST)){
 }
 echo "<h1>" . getString("site_name") . "</h1>";
 echo "<hr>";
+echo "<h2>Nouveautés de la version 2.1</h2>
+    <p>
+        Le 2 septembre 2024, le site a reçu une mise à jour majeure : la version 2.1.<br><br>
+        Cette version apporte, outre les multiples corrections mineures, une grande nouveauté : le support multilingue !<br>
+        Le site est déjà disponible en anglais (bêta). N'hésitez pas à améliorer les traductions et/ou à traduire le site dans d'autres langues en vous rendant sur la page <a href=\"https://crowdin.com/project/better-twitch-predictions\">Crowdin</a> du projet !<br>
+        Si vous souhaitez traduire le site dans une langue autre que celles présentes pour l'instant, contactez le développeur (voir page \"À propos\")<br>
+    </p>
+    <hr>";
 $predictions = arraySQL("SELECT `id`, `title` FROM `predictions` WHERE `ended` > NOW() ORDER BY `ended` ASC;");
 $predictions_count = $predictions?count($predictions):0;
 echo "<h2>" . getString("predictions_ongoing", [$predictions_count]) . "</h2>";
