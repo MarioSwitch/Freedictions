574ef62bff920beecfb4eb020a4d6e45cb8bc707
MarioSwitch <58565980+MarioSwitch@users.noreply.github.com>
2024-09-24 17:31:10 +0200

    [2.1.38] Tweak settings display

--- views/settings.php
+++ views/settings.php
@@ -12,26 +12,31 @@ $sln = getSetting("sln");
 echo "
 <h1>" . getString("settings_title") . "</h1>
 <form role=\"form\" action=\"controller.php\">
-    <p>
-        <label for=\"language\">" . getString("settings_language") . "</label>
-        <select name=\"language\" id=\"language\">
-            <option value=\"fr\"" . ($language=="fr"?" selected":"") . ">" . getString("settings_language_fr") . " " . getString("settings_default") . "</option>
-            <option value=\"en\"" . ($language=="en"?" selected":"") . ">" . getString("settings_language_en") . " " . getString("settings_beta") . "</option>
-            <option value=\"de\"" . ($language=="de"?" selected":"") . ">" . getString("settings_language_de") . " " . getString("settings_beta") . "</option>
-        </select>
-        <br/>
-        <small>" . getString("settings_language_description") . "</small>
-    </p>
-    <br/>
-    <p>
-        <label for=\"sln\">" . getString("settings_sln") . "</label>
-        <select name=\"sln\" id=\"sln\">
-            <option value=\"yes\"" . ($sln=="yes"?" selected":"") . ">" . getString("settings_yes") . " " . getString("settings_default") . "</option>
-            <option value=\"no\"" . ($sln=="no"?" selected":"") . ">" . getString("settings_no") . "</option>
-        </select>
-        <br/>
-        <small>" . getString("settings_sln_description") . "</small>
-    </p>
+    <table id=\"settings\">
+        <tr>
+            <td>
+                <label for=\"language\"><abbr title=\"" . getString("settings_language_description") . "\">" . getString("settings_language") . "*</abbr></label>
+            </td>
+            <td>
+                <select name=\"language\" id=\"language\">
+                    <option value=\"fr\"" . ($language=="fr"?" selected":"") . ">" . getString("settings_language_fr") . " " . getString("settings_default") . "</option>
+                    <option value=\"en\"" . ($language=="en"?" selected":"") . ">" . getString("settings_language_en") . " " . getString("settings_beta") . "</option>
+                    <option value=\"de\"" . ($language=="de"?" selected":"") . ">" . getString("settings_language_de") . " " . getString("settings_beta") . "</option>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <label for=\"sln\"><abbr title=\"" . getString("settings_sln_description") . "\">" . getString("settings_sln") . "*</abbr></label>
+            </td>
+            <td>
+                <select name=\"sln\" id=\"sln\">
+                    <option value=\"yes\"" . ($sln=="yes"?" selected":"") . ">" . getString("settings_yes") . " " . getString("settings_default") . "</option>
+                    <option value=\"no\"" . ($sln=="no"?" selected":"") . ">" . getString("settings_no") . "</option>
+                </select>
+            </td>
+        </tr>
+    </table>
     <br/>
     <button type=\"submit\" name=\"action\" value=\"settings\">" . getString("settings_apply") . "</button>
 </form>";
