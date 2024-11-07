894af3a344a097e7fc96847a57be3dfca4461c94
MarioSwitch <58565980+MarioSwitch@users.noreply.github.com>
2024-11-07 14:26:11 +0100

    [3.0.0-alpha1] Added RewriteRule

--- index.php
+++ index.php
@@ -1,20 +1,2 @@
 <?php
-include_once "functions.php";
-
-if(!array_key_exists("view", $_REQUEST)){
-    header("Location:index.php?view=home");
-    die();
-}
-
-$view = $_REQUEST["view"];
-
-if(!file_exists("views/$view.php")){
-    header("Location:index.php?view=home");
-    die();
-}
-
-include "header.php";
-
-include "views/$view.php";
-
-echo "<br>";
+    print_r($_REQUEST);
