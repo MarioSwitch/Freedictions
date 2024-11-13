99ae77bf33b735c952819529ca28c24518fc51d5
MarioSwitch <58565980+MarioSwitch@users.noreply.github.com>
2024-11-13 11:08:05 +0100

    [3.0.0-alpha7] Added displayRank, displayFloat and created controller

--- index.php
+++ index.php
@@ -7,3 +7,7 @@ echo "<br>";
 echo displayUser("MarioSwitch", true);
 echo "<br>";
 echo displayInt(-1234567, true, true);
+echo "<br>";
+echo displayRank(2);
+echo "<br>";
+echo displayFloat(-150.9);
