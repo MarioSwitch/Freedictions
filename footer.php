bd5dc930f8f4568e26e719fe250fd801c8b90242
MarioSwitch <58565980+MarioSwitch@users.noreply.github.com>
2024-11-14 14:46:49 +0100

    [3.0.0-alpha11] Header + footer + some pages

---
+++ footer.php
@@ -0,0 +1,8 @@
+<?php
+echo "
+	<div id=\"footer\">
+		<p>Initialement créé par MarioSwitch et Yosh</p>
+		<p>Code disponible sur <a href=\"https://github.com/MarioSwitch/BetterTwitchPredictions\">GitHub</a></p>
+		<p>v3.0.0-alpha10 du 14 nov. 2024</p>
+	</div>
+";
