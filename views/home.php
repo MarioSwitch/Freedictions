4a920a388e7a797b1e2db5a42321536d52b75dcc
MarioSwitch <58565980+MarioSwitch@users.noreply.github.com>
2024-12-03 15:32:44 +0100

    [3.0-a23] Home + user pages

--- views/home.php
+++ views/home.php
@@ -1,25 +1,67 @@
 <?php
+/**
+ * Génère le code HTML pour afficher une liste de prédictions
+ * @param array $predictions Prédictions à afficher
+ * @return string Code HTML
+ */
+function displayPredictionsList(array $predictions): string{
+	$already_bet = array_key_exists("name", $predictions[0]) && array_key_exists("chips", $predictions[0]);
+	$html = "
+	<table class=\"predictions_list\">
+		<thead>
+			<tr>
+				<th>" . getString("predictions_table_question") . "</th>
+				<th>" . getString("predictions_table_participation") . "</th>";
+				$html .= $already_bet ? "<th>" . getString("predictions_table_bet") . "</th>" : "";
+				$html .= "<th>" . getString("predictions_table_ended") . "</th>
+			</tr>
+		</thead>
+		<tbody>";
+		foreach($predictions as $prediction){
+			$id = $prediction["id"];
+			$title = $prediction["title"];
+			$ended = $prediction["ended"];
+			$users = executeQuery("SELECT COUNT(*) FROM `bets` WHERE `prediction` = ?;", [$prediction["id"]], "int");
+			$volume = executeQuery("SELECT SUM(`chips`) FROM `bets` WHERE `prediction` = ?;", [$prediction["id"]], "int");
+			$bet_name = $already_bet ? $prediction["name"] : "";
+			$bet_chips = $already_bet ? $prediction["chips"] : "";
+
+			$html .= "
+			<tr>
+				<td><a href=\"../prediction/$id\">$title</a></td>
+				<td>" . displayInt($users) . insertTextIcon("users", "right", 1) . "<br>" . displayInt($volume) . insertTextIcon("chips", "right", 1) . "</td>";
+				$html .= $already_bet ? "<td>$bet_name<br>" . displayInt($bet_chips) . insertTextIcon("chips", "right", 1) . "</td>" : "";
+				$html .= "<td id=\"ended_$id\">$ended</td>
+				<script>display(\"$ended\",\"ended_$id\")</script>
+			</tr>
+			";
+		}
+		$html .= "
+		</tbody>
+	</table>";
+	return $html;
+}
+
 $opened = executeQuery("SELECT `id`, `title`, `ended` FROM `predictions` WHERE `ended` > NOW() ORDER BY `ended` ASC;");
 $count = count($opened);
 include_once "time.js.php";
-echo "<h1>" . getString("predictions_opened") . " (" . $count . ")</h1>";
+echo "<h1>" . getString("predictions_opened") . " (" . displayInt($count) . ")</h1>";
 if($count == 0) echo "<p>" . getString("predictions_none") . "</p>";
-foreach($opened as $prediction){
-	$id = $prediction["id"];
-	$title = $prediction["title"];
-	$ended = $prediction["ended"];
-	$volume = executeQuery("SELECT SUM(`chips`) FROM `bets` WHERE `prediction` = ?;", [$prediction["id"]], "int");
+if($count > 0){
+	if(!isConnected()) echo displayPredictionsList($opened);
+	if(isConnected()){
+		$not_bet = executeQuery("SELECT `predictions`.`id`, `predictions`.`title`, `predictions`.`ended` FROM `predictions` WHERE `predictions`.`ended` > NOW() AND `predictions`.`id` NOT IN (SELECT `predictions`.`id` FROM `predictions` JOIN `choices` ON `choices`.`prediction` = `predictions`.`id` JOIN `bets` ON `bets`.`choice` = `choices`.`id` WHERE `bets`.`user` = ? AND `answer` IS NULL) ORDER BY `predictions`.`ended` ASC;", [$_COOKIE["username"]]);
+		$not_bet_count = count($not_bet);
+		echo "<h2>" . getString("predictions_opened_bet_none") . " (" . displayInt($not_bet_count) . ")</h2>";
+		if($not_bet_count == 0) echo "<p>" . getString("predictions_none") . "</p>";
+		if($not_bet_count > 0) echo displayPredictionsList($not_bet);
 
-	echo "
-	<a href=\"" . CONFIG_PATH . "/prediction/$id\">
-		<p style=\"font-size:calc(var(--font-size) * 1.5); margin:0;\">$title</p>
-		<div style=\"width:calc(var(--font-size) * 12); display:inline-block;\">
-			<p style=\"margin:0; text-align:left; float:left;\">" . insertTextIcon("chips", "left", 1.2) . displayInt($volume) . "</p>
-			<p style=\"margin:0; text-align:right;\"><span id=\"end_$id\">$ended</span>" . insertTextIcon("sandglass", "right", 1.2) . "</p>
-		</div>
-	<script>display(\"" . $prediction["ended"] . "\", \"end_" . $prediction["id"] . "\");</script>
-	</a>
-	<br>
-	<hr class=\"mini\">
-	";
+		echo "<br><br>";
+
+		$already_bet = executeQuery("SELECT `predictions`.`id`, `predictions`.`title`, `predictions`.`ended`, `choices`.`name`, `bets`.`chips` FROM `predictions` JOIN `choices` ON `choices`.`prediction` = `predictions`.`id` JOIN `bets` ON `bets`.`choice` = `choices`.`id` WHERE `bets`.`user` = ? AND NOW() < `ended` AND `answer` IS NULL ORDER BY `predictions`.`ended` ASC;", [$_COOKIE["username"]]);
+		$already_bet_count = count($already_bet);
+		echo "<h2>" . getString("predictions_opened_bet_already") . " (" . displayInt($already_bet_count) . ")</h2>";
+		if($already_bet_count == 0) echo "<p>" . getString("predictions_none") . "</p>";
+		if($already_bet_count > 0) echo displayPredictionsList($already_bet);
+	}
 }
