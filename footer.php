f60d7fe9f8c051a537518b21375b4513bc5b96a6
MarioSwitch <58565980+MarioSwitch@users.noreply.github.com>
2024-12-04 16:53:52 +0100

    [3.0-a27] Create prediction control + started prediction page

--- footer.php
+++ footer.php
@@ -2,5 +2,5 @@
 	<p><?= getString("footer_1", ["<b><a href=\"https://github.com/MarioSwitch\">MarioSwitch</a></b>", "<b><a href=\"https://github.com/yoshakami\">yoshakami</a></b>"]) ?></p>
 	<p><?= getString("footer_2", ["<b><a href=\"https://github.com/MarioSwitch/BetterTwitchPredictions\">GitHub</a></b>"]) ?></p>
 	<p><?= getString("footer_3", ["<b><a href=\"https://discord.gg/PCKx4qf9XZ\">", "</a></b>"]) ?></p>
-	<p>Build 3.0-a26 (2024-12-04)</p>
+	<p>Build 3.0-a27 (2024-12-04)</p>
 </div>
