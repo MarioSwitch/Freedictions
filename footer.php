d10d05c520fc45bdcc6769089fc03c8b3db03766
MarioSwitch <58565980+MarioSwitch@users.noreply.github.com>
2024-12-09 17:16:15 +0100

    [3.0-a33] Updated to Freedictions

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div id="footer">
 	<p><?= getString("footer_1", ["<b><a href=\"https://github.com/MarioSwitch\">MarioSwitch</a></b>", "<b><a href=\"https://github.com/yoshakami\">yoshakami</a></b>"]) ?></p>
-	<p><?= getString("footer_2", ["<b><a href=\"https://github.com/MarioSwitch/BetterTwitchPredictions\">GitHub</a></b>"]) ?></p>
+	<p><?= getString("footer_2", ["<b><a href=\"https://github.com/MarioSwitch/Freedictions\">GitHub</a></b>"]) ?></p>
 	<p><?= getString("footer_3", ["<b><a href=\"https://discord.gg/PCKx4qf9XZ\">", "</a></b>"]) ?></p>
-	<p>Build 3.0-a32 (2024-12-06)</p>
+	<p>Build 3.0-a33 (2024-12-09)</p>
 </div>
