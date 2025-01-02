98279085a2ed8b76f54011a7fbbb1f37b88279d0
MarioSwitch <58565980+MarioSwitch@users.noreply.github.com>
2025-01-02 15:39:03 +0100

    [3.0.64-alpha] Added ability to change password

---
+++ views/password.php
@@ -0,0 +1,26 @@
+<?php
+$username_connected = $_COOKIE["username"];
+$username_concerned = $_REQUEST["user"];
+
+$user_exists = count(executeQuery("SELECT * FROM `users` WHERE `username` = ?", [$username_concerned]));
+if(!$user_exists) redirect("home");
+
+$perms = isMod() || $username_connected == $username_concerned;
+if(!$perms) redirect("user/$username_concerned", "perms");
+?>
+<h1><?= getString("title_password") ?></h1>
+<form role="form" action="controller.php">
+	<label for="user"><?= getString("general_user") ?></label>
+	<input type="text" name="user" id="user" value="<?= $username_concerned ?>" required="required" readonly="readonly">
+	<br>
+	<label for="pv"><?= getString("password_current") ?></label>
+	<input type="password" name="pv" id="pv" required="required">
+	<br>
+	<label for="np"><?= getString("password_new") ?></label>
+	<input type="password" name="np" id="np" required="required">
+	<br>
+	<label for="np_confirm"><?= getString("password_new_confirm") ?></label>
+	<input type="password" name="np_confirm" id="np_confirm" required="required">
+	<br>
+	<button type="submit" name="action" value="change_password"><?= getString("general_save") ?></button>
+</form>
