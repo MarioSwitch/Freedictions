0770b2946c955b639da38156229a444a77747c3d
MarioSwitch <MarioSwitch@users.noreply.github.com>
2025-04-22 11:14:48 +0200

    [3.0.67-alpha] Added German + localization fixes

--- footer.php
+++ footer.php
@@ -2,5 +2,5 @@
 	<p><?= getString("footer_1", ["<b><a href=\"https://github.com/MarioSwitch\">MarioSwitch</a></b>", "<b><a href=\"https://github.com/yoshakami\">yoshakami</a></b>"]) ?></p>
 	<p><?= getString("footer_2", ["<b><a href=\"https://github.com/MarioSwitch/Freedictions\">GitHub</a></b>"]) ?></p>
 	<p><?= getString("footer_3", ["<b><a href=\"https://discord.gg/PCKx4qf9XZ\">", "</a></b>"]) ?></p>
-	<p>Build 3.0.64-alpha (2025-01-02)</p>
+	<p>Build 3.0.67-alpha (2025-04-22)</p>
 </div>
