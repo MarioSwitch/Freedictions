a7b4f4c36ba3c7b613d1c0768062e1a8cdc33ec8
MarioSwitch <druonalexis@outlook.fr>
2025-06-09 10:48:10 +0200

    [3.0.70-alpha] Fixed DE + FR strings

--- footer.php
+++ footer.php
@@ -2,5 +2,5 @@
 	<p><?= getString("footer_1", ["<b><a href=\"https://github.com/MarioSwitch\">MarioSwitch</a></b>", "<b><a href=\"https://github.com/yoshakami\">yoshakami</a></b>"]) ?></p>
 	<p><?= getString("footer_2", ["<b><a href=\"https://github.com/MarioSwitch/Freedictions\">GitHub</a></b>"]) ?></p>
 	<p><?= getString("footer_3", ["<b><a href=\"https://discord.gg/PCKx4qf9XZ\">", "</a></b>"]) ?></p>
-	<p>Build 3.0.68-alpha (2025-04-22)</p>
+	<p>Build 3.0.70-alpha (2025-06-09)</p>
 </div>
