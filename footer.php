8db87ca848aca867608f1054e5441aa0fab5b369
MarioSwitch <druonalexis@outlook.fr>
2025-06-09 12:20:43 +0200

    [3.0.74-alpha] Fixed RewriteRules

--- footer.php
+++ footer.php
@@ -2,5 +2,5 @@
 	<p><?= getString("footer_1", ["<b><a href=\"https://github.com/MarioSwitch\">MarioSwitch</a></b>", "<b><a href=\"https://github.com/yoshakami\">yoshakami</a></b>"]) ?></p>
 	<p><?= getString("footer_2", ["<b><a href=\"https://github.com/MarioSwitch/Freedictions\">GitHub</a></b>"]) ?></p>
 	<p><?= getString("footer_3", ["<b><a href=\"https://discord.gg/PCKx4qf9XZ\">", "</a></b>"]) ?></p>
-	<p>Build 3.0.71-alpha (2025-06-09)</p>
+	<p>Build 3.0.74-alpha (2025-06-09)</p>
 </div>
